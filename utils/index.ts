9493c294be9c52ebe9b76a6344bcb76b51f9c64e
Syed Khaja Navaz Ali <sknavaz29@gmail.com>
2023-08-21 01:47:59 +0530

    final

--- utils/index.ts
+++ utils/index.ts
@@ -1,38 +1,14 @@
 import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
-    // const headers = {
-    //     'X-RapidAPI-Key': '4a73b03bc4msh44878d00f6b4e71p116a81jsnd7fb26bdf860',
-    //      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-    // }
-
-    // const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
-    //     headers: headers,
-    // });
-
-    const headers = {
-        'X-API-Key': '9LhPF7VDSQqak2q++66SVA==GDlcDgZ1eZVFulRi'
-    }
-
-    const response = await fetch('https://api.api-ninjas.com/v1/cars?model=q3',{
-        headers: headers,
-    });
-    
-    const result = await response.json();
-
-    return result;
-}
-
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 1000; // Base rental price per day in dollars
-    const mileageFactor = 0.1; // Additional rate per mile driven
-    const ageFactor = 0.05; // Additional rate per year of vehicle age
+    const basePricePerDay = 1000;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05; 
   
-    // Calculate additional rate based on mileage and age
+
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
-    // Calculate total rental rate per day
+
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
   
     return rentalRatePerDay.toFixed(0);
@@ -51,4 +27,53 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     url.searchParams.append('angle', `${angle}`);
 
     return `${url}`;
+}
+
+
+export const updateSearchParams = (type: string, value: string) => {
+
+    const searchParams = new URLSearchParams(window.location.search);
+   
+    searchParams.set(type, value);
+  
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  
+    return newPathname;
+  };
+  
+  export const deleteSearchParams = (type: string) => {
+    const newSearchParams = new URLSearchParams(window.location.search);
+
+    newSearchParams.delete(type.toLocaleLowerCase());
+
+    const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+  
+    return newPathname;
+  };
+
+  
+export async function fetchCars(filters:FilterProps) {
+    // const headers = {
+    //     'X-RapidAPI-Key': '4a73b03bc4msh44878d00f6b4e71p116a81jsnd7fb26bdf860',
+    //      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    // }
+
+    // const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
+    //     headers: headers,
+    // });
+
+    const { manufacturer, year, model, limit, fuel } = filters;
+
+    const headers = {
+        'X-API-Key': '9LhPF7VDSQqak2q++66SVA==GDlcDgZ1eZVFulRi'
+    }
+
+    const response = await fetch(`https://api.api-ninjas.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{
+        headers: headers,
+    });
+    
+    const result = await response.json();
+
+    return result;
 }
